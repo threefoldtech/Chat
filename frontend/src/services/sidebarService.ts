6c4dc2322dc74de73c4b2338bd92ccbf51c66cd7
JensMeire <meire.jens>
2021-04-07 10:48:36 +0200

    [Bug] Info aside not showing anymore

--- frontend/src/services/sidebarService.ts
+++ frontend/src/services/sidebarService.ts
@@ -1,6 +1,7 @@
-import { ref } from 'vue';
-export const showSideBar = ref(false);
+import { readonly, ref } from 'vue';
+const showSideBar = ref(false);
 
+export const getShowSideBar = () => readonly(showSideBar);
 export const toggleSideBar = () => {
     showSideBar.value = !showSideBar.value;
-}
+};
